9c41575c916c7aa3a6f049852b1f23809b72a329
Ayush Gupta <72538480+GuptaAyush19@users.noreply.github.com>
2021-11-07 14:12:19 +0530

    Include live README.md in /about

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+def readme():
+    import requests
+    from bs4 import BeautifulSoup
+
+    page = requests.get('https://github.com/GuptaAyush19/AuraX/blob/develop/README.md')
+    soup = BeautifulSoup(page.content, 'html.parser') 
+
+    return soup.find(id='readme')
+
 @app.route('/')
 def index():
     return render_template('index.html'), 200
 
 @app.route('/about')
 def about():
-    return render_template('about.html'), 200
+    return render_template('about.html', about=readme()), 200
